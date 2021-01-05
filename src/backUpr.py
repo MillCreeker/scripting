fc3cfcdd0d9212deadfe80a2162bee00ac5b6909
MillCreeker <59824109+MillCreeker@users.noreply.github.com>
2021-01-05 14:40:48 +0100

    implemented backUpr.py
    - returns all the files that require a backup
    - gets this information from the .config file

--- src/backUpr.py
+++ src/backUpr.py
@@ -1,7 +1,63 @@
 #!/usr/bin/env python3
 
-config = open(".config", "r")
-to_backup = config.readlines()
+from os import listdir
+from os.path import isfile, join
 
-for file in to_backup:
-    pass
+# reads all settings and converts them into convenient lists
+def get_contents(tag_name, text):
+    content = []
+    head = False
+    body = False
+    
+    for line in text:
+        if line.strip():
+            if line.__contains__("}") and head == True and body == True:
+                head = False
+                body = False
+        
+            if head == True and body == True:
+                content.append(line.strip())
+            
+            if line.__contains__("\""+tag_name+"\":") and head == False and body == False:
+                head = True
+            if line.__contains__("{") and head == True and body == False:
+                body = True
+    
+    return content
+
+def get_backup_files_list():
+    config = open(".config", "r")
+    config_text = config.readlines()
+    config.close()
+        
+    match   = get_contents("match", config_text)
+    include = get_contents("include", config_text)
+    exclude = get_contents("exclude", config_text)
+
+
+    # gets a list of all the files which require a back-up
+    files_path = "files"
+    files = [f for f in listdir(files_path) if isfile(join(files_path, f))]
+
+    files_to_backup = []
+
+    # match
+    for element in match:
+        for file in files:
+            if file.__contains__(element):
+                files_to_backup.append(file)
+
+    # include
+    for element in include:
+        for file in files:
+            if file == element:
+                files_to_backup.append(element)
+    files_to_backup = list(dict.fromkeys(files_to_backup))
+
+    # exclude
+    for element in exclude:
+        for file in files:
+            if file == element:
+                files_to_backup.remove(element)
+
+    return files_to_backup
