a566efb99cba6ede1f8d4fc9da603fe7f6ce658b
Sebastian Toporsch <s.toporsch@conversory.net>
2021-01-29 15:33:44 +0100

    reverted to static *

--- src/cronjob.sh
+++ src/cronjob.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" #Changes directory to directory of this file
-echo "$MINUTE $HOUR $DAY $MONTH $DAYOFWEEK cd $DIR && python logger.py" | crontab -                #Adds command to crontab
+echo "* * * * * cd $DIR && python logger.py" | crontab -                #Adds command to crontab
