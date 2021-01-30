fef77d81528a89e48f39bab383ccb5eb2422d83b
Sebastian Toporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-01-30 22:38:45 +0100

    removed unecessary comment

--- cronjob.sh
+++ cronjob.sh
@@ -2,6 +2,6 @@
 #Changes directory to directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 #Adds command to crontab.
-#$1 = minutes, $2 = hours, $3 = days, $4 = hours, $5 = hours
+#$1 = minutes, $2 = hours, $3 = days
 echo "$1 $2 $3 * * cd $DIR && python src/Main.py" | crontab -  
 #echo "* * * * * cd $DIR && python3 src/Logger.py" | crontab -             
